8be45d83d27b483d1b002e928b0b2b4cbcb01a7e
eternal golden bread <codylewisbrock@gmail.com>
2019-10-13 00:25:43 -0700

    continues work on the constructor. added in the image source.  html now has redundancy on that

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,28 @@ var name = "";
 var hp = 0;
 var attack = 0;
 var counter = 0;
+var img;
 var charArr = [];
 
-function Setup(charName, hp, attack, counter) {
+function Setup(charName, hp, attack, counter, picSource) {
     this.name = charName;
     this.hp = hp;
     this.attack = attack;
-    this.counter = counter
+    this.counter = counter;
+    this.picSource = picSource;
     charArr.push(this);
 }
 
 function initialize() {
-    var mouse = new Setup("mouse",120,25,20);
-    var willrow = new Setup("willrow",150,20,15);
-    var porkins = new Setup("porkins", 100,20,10);
-    var figrin = new Setup("figrin", 160,35,20);    
+    var mouse = new Setup("mouse",120,25,20, "assets/images/mouse-droid.jpeg");
+    var willrow = new Setup("willrow",150,20,15, "assets/images/willrow-hood.jpg");
+    var porkins = new Setup("porkins", 100,20,10, "assets/images/porkins-2.webp");
+    var figrin = new Setup("figrin", 160,35,20, "assets/images/figrin-dan.jpg");    
 }
 
 initialize();
 
-console.log(charArr[0]);
+console.log(charArr[3].picSource);
 
 
 
@@ -30,5 +32,5 @@ console.log(charArr[0]);
 
 
 $(".character").on("click", function() {
-    alert("you've clicked the mouse!");
+    console.log('you have clicked on ' + $("this.hp") + '!');
 })
