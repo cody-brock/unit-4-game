338e9d1c82edf345903cc65d8aff04f8e474aca2
eternal golden bread <codylewisbrock@gmail.com>
2019-10-16 07:47:49 -0700

    wraps the whole game in an onready function.  sets up restart button so it can restart the game fully

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+$(document).ready(function initialize() {
+    console.log("ready");
+
 var name = "";
 var hp = 0;
 var attack = 0;
@@ -8,122 +11,143 @@ var playerChosen = false;
 var defenderSelected = false;
 var player;
 var defender;
+$("#characters-div").empty();
+$("#your-character").empty();
+$("#enemies-div").empty();
+$("#defender-div").empty();
+$("#messages").empty();
+$("#restart-button").css("display", "none");
 
-//Constructor to give characters their attributes
-class Character {
-    constructor(name,hp,ap,ca,picSrc){
-        this.name = name;
-        this.hp = hp;
-        this.ap = ap;
-        this.ca = ca;
-        this.picSrc = picSrc;
-        charArr.push(this);
-    }
-}
 
-//gives the constructor the info it needs
-var mouse = new Character("mouse",120,15,20,"assets/images/mouse-droid.jpeg");
-var willrow = new Character("willrow",150,10,15, "assets/images/willrow-hood.jpg");
-var porkins = new Character("porkins", 100,10,10, "assets/images/porkins-2.webp");
-var figrin = new Character("figrin", 160,15,20, "assets/images/figrin-dan.jpg"); 
 
-//pushes the character images onto the page, adds class and id
-function makeImgDiv (x, destination) {
-    x.forEach(element => {
-        var charImg = $("<img>");
-        charImg.addClass("characters");
-        charImg.attr("src", element.picSrc);
-        charImg.attr("id", element.name);
-        // $("#characters-div").append(charImg);
-        $(destination).append(charImg);
-        charImg.append(element.name + "<br>")
-        // console.log(charImg);
-        charImg.append()
-    });
-}
-
-makeImgDiv(charArr, "#characters-div")
-
-//Choose character and move others into Enemies div
-$(".characters").on("click", function () {
-    if (!playerChosen) {
-        for (let i=0; i<charArr.length; i++) {
-            if (charArr[i].name === this.id) {
-                playerChosen = true;
-                player = charArr[i];
-                charArr.splice(i, 1);
-                $("#your-character").append(this);
-            }
+    //Constructor to give characters their attributes
+    class Character {
+        constructor(name,hp,ap,ca,picSrc){
+            this.name = name;
+            this.hp = hp;
+            this.ap = ap;
+            this.ca = ca;
+            this.picSrc = picSrc;
+            charArr.push(this);
         }
+    }
 
-        for (let i=0; i<charArr.length; i++) {
-            var charImg = $("<img>")
-            charImg.removeClass("characters");
-            charImg.addClass("enemies");
-            charImg.attr("src", charArr[i].picSrc);
-            charImg.attr("id", charArr[i].name)
-            $("#enemies-div").append(charImg);
-            $("#characters-div").empty();
-        }    
-    }   
+    //gives the constructor the info it needs
+    var mouse = new Character("mouse",120,15,20,"assets/images/mouse-droid.jpeg");
+    var willrow = new Character("willrow",150,10,15, "assets/images/willrow-hood.jpg");
+    var porkins = new Character("porkins", 100,10,10, "assets/images/porkins-2.webp");
+    var figrin = new Character("figrin", 160,15,70, "assets/images/figrin-dan.jpg"); 
 
-    //choose defender, move to Defender div
-    $(".enemies").on("click", function () {
-        if (!defenderSelected) {
+    //pushes the character images onto the page, adds class and id
+    function makeImgDiv (x, destination) {
+        x.forEach(element => {
+            var charImg = $("<img>");
+            charImg.addClass("characters");
+            charImg.attr("src", element.picSrc);
+            charImg.attr("id", element.name);
+            // $("#characters-div").append(charImg);
+            $(destination).append(charImg);
+            charImg.append(element.name + "<br>")
+            // console.log(charImg);
+            charImg.append()
+        });
+    }
+
+    makeImgDiv(charArr, "#characters-div")
+
+    //Choose character and move others into Enemies div
+    $(".characters").on("click", function () {
+        if (!playerChosen) {
             for (let i=0; i<charArr.length; i++) {
                 if (charArr[i].name === this.id) {
-                    var charImg = $("<img>")
-                    charImg.removeClass("enemies");
-                    charImg.addClass("defender");
-                    $("#defender-div").append(this);
-                    defenderSelected = true;
-                    defender = charArr[i];
+                    playerChosen = true;
+                    player = charArr[i];
+                    charArr.splice(i, 1);
+                    $("#your-character").append(this);
                 }
             }
+
+            for (let i=0; i<charArr.length; i++) {
+                var charImg = $("<img>")
+                charImg.removeClass("characters");
+                charImg.addClass("enemies");
+                charImg.attr("src", charArr[i].picSrc);
+                charImg.attr("id", charArr[i].name)
+                $("#enemies-div").append(charImg);
+                $("#characters-div").empty();
+            }    
+        }   
+
+        //choose defender, move to Defender div
+        $(".enemies").on("click", function () {
+            if (!defenderSelected) {
+                for (let i=0; i<charArr.length; i++) {
+                    if (charArr[i].name === this.id) {
+                        var charImg = $("<img>")
+                        charImg.removeClass("enemies");
+                        charImg.addClass("defender");
+                        $("#defender-div").append(this);
+                        defenderSelected = true;
+                        defender = charArr[i];
+                        $("#messages").empty();
+                    }
+                }
+            }
+        })
+    })
+
+
+    //function to increment player's attack power
+    var playerAttackIncrease = function(player) {
+        player.ap = player.ap + player.ap;
+    }
+
+
+    //function for handling the logic behind the attack button conditions
+    var attackFun = function(player, defender) {
+        //if both characters have hp left...
+        if (player.hp > 0 && defender.hp > 0) {
+            //first player attacks defender
+            defender.hp = defender.hp - player.ap;
+            //increments the player's attack power
+            playerAttackIncrease(player);
+            //if defender still has hp...
+            if (defender.hp > 0) {
+                player.hp = player.hp - defender.ca;
+
+            } 
+            if (player.hp <= 0) {
+                $("#messages").text("You have been defeated...GAME OVER!!!");
+                $("#restart-button").css("display", "block");
+                // console.log("hello?")
+            }
+            //if defender has no hp, victory conditions met, remove defender
+            //and allow new one to be selected
+            else if (defender.hp <= 0) {
+                $("#messages").text("You have defeated " + defender.name + ". You can choose to fight another enemy.")
+                $("#defender-div").empty();
+                defenderSelected = false;
+            }
+        }
+        console.log(player.hp, defender.hp);
+    }
+
+    //when "attack" button is pressed...
+    $("#fight-button").on("click", function () {
+        if (playerChosen && defenderSelected) {
+            attackFun(player, defender);
         }
     })
+
+    //restart button event handler
+    $("#restart-button").on("click", function() {
+        initialize();
+    })
+
+
+
+
 })
-
-
-//function to increment player's attack power
-var playerAttackIncrease = function(player) {
-    player.ap = player.ap + player.ap;
-}
-
-//function for handling the logic behind the attack button conditions
-var attackFun = function(player, defender) {
-    //if both characters have hp left...
-    if (player.hp > 0 && defender.hp > 0) {
-        //first player attacks defender
-        defender.hp = defender.hp - player.ap;
-        //increments the player's attack power
-        playerAttackIncrease(player);
-        //if defender still has hp...
-        if (defender.hp > 0) {
-            player.hp = player.hp - defender.ca;
-        } 
-        //if defender has no hp, victory conditions met, remove defender
-        //and allow new one to be selected
-        else if (defender.hp <= 0) {
-            console.log("victory");
-            $("#defender-div").empty();
-            defenderSelected = false;
-        }
-    }
-    console.log(player.hp, defender.hp);
-}
-
-//when "attack" button is pressed...
-$("#fight-button").on("click", function () {
-    if (playerChosen && defenderSelected) {
-        attackFun(player, defender);
-    }
-})
-
-
-
-
-    
     
         
             
