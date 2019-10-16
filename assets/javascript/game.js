3b03a7871a714adbfea4b2b49aa448235759da41
eternal golden bread <codylewisbrock@gmail.com>
2019-10-15 20:33:07 -0700

    lots of changes.  Most importantly, the onclick function now works to move characters into the player div and enemies div.  some minor styling applied.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,11 @@ var attack = 0;
 var counter = 0;
 var img;
 var charArr = [];
+var playerChosen = false;
+var defenderSelected = false;
+var player
 
-
+//Constructor to give characters their attributes
 class Character {
     constructor(name,hp,ap,ca,picSrc){
         this.name = name;
@@ -17,29 +20,84 @@ class Character {
     }
 }
 
-var mouse = new Character('mouse',120,25,20,"assets/images/mouse-droid.jpeg");
+//gives the constructor the info it needs
+var mouse = new Character("mouse",120,25,20,"assets/images/mouse-droid.jpeg");
 var willrow = new Character("willrow",150,20,15, "assets/images/willrow-hood.jpg");
 var porkins = new Character("porkins", 100,20,10, "assets/images/porkins-2.webp");
 var figrin = new Character("figrin", 160,35,20, "assets/images/figrin-dan.jpg"); 
 
-console.log(mouse.picSrc);
+//pushes the character images onto the page, adds class and id
+function makeImgDiv (x, destination) {
+    x.forEach(element => {
+        var charImg = $("<img>");
+        charImg.addClass("characters");
+        charImg.attr("src", element.picSrc);
+        charImg.attr("id", element.name);
+        // $("#characters-div").append(charImg);
+        $(destination).append(charImg);
+        charImg.append(element.name + "<br>")
+        // console.log(charImg);
+        charImg.append()
+    });
+}
 
-charArr.forEach(element => {
-    console.log("wow")
-    var charImg = $("<img>");
-    charImg.addClass("character");
-    charImg.attr("src", element.picSrc);
-    $("#characters-div").append(charImg);
-});
+makeImgDiv(charArr, "#characters-div")
 
+//Choose character and move others into Enemies div
+$(".characters").on("click", function () {
+    if (!playerChosen) {
+        for (let i=0; i<charArr.length; i++) {
+            if (charArr[i].name === this.id) {
+                playerChosen = true;
+                player = this.id;
+                charArr.splice(i, 1);
+                $("#your-character").append(this);
+            }
+        }
 
-
-// console.log(mouse);
-
-// console.log(charArr);
-
-var attackTally = 0;
-$(".character").on("click", function() {
-    attackTally += 10;
-    console.log('you have dealt ' + attackTally + ' damage!');
+        for (let i=0; i<charArr.length; i++) {
+            var charImg = $("<img>")
+            charImg.addClass("enemies")
+            charImg.attr("src", charArr[i].picSrc);
+            charImg.attr("id", charArr[i].name)
+            $("#enemies-div").append(charImg);
+            $("#characters-div").empty();
+        }    
+    }   
 })
+
+//choose defender, move to Defender div
+$(".enemies").on("click", function () {
+    console.log("anyone home")
+    $("#defender-div").append(this);
+})
+    
+    
+        
+            
+   
+
+
+//moves characters from place to place
+
+
+
+// console.log(charArr[0]);
+// console.log("outside click", mouse.ap);
+
+//Choose character
+// $(".characters").on("click", function() {
+//     console.log("inside click", $( this ).ap);
+//     console.log(($(this).attr("name")));
+// });
+
+// // console.log(mouse.name)
+
+
+// $(".character").on("click", function() {
+//     // let attackPoints = ${this.name};
+//     console.log("${this.name}");
+//     // attackTally += this.ap;
+//     // console.log('you have dealt ' + attackTally + ' damage!');
+// });
+
